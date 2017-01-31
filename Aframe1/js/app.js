d11bf8f5240c8a33dd8a3a0c7774735db46d2843
Jessica Yu <jessica.yu@gmail.com>
2017-01-31 00:17:09 -0500

    fixing the geolocation

--- Aframe1/js/app.js
+++ Aframe1/js/app.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
-
+// var aframeparticle = require('aframe-particle-system-component');
 ////---------- CONSTANTS
 	var CONSTANTS = {
-		API_KEY: "",
 		BASE_URL: "http://api.wunderground.com/api/341eced3dbad667b/geolookup/conditions/q/IA/",
+		GOOGLE_URL:"https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyAhAF8TYcKRz0GxKFJM3zzKWtgZ7PvWy3s",
+
 
 //arrays of weather icons displayed on wunderground and what i will display. structured so i can move them around, make more display options later
 		showCloud: ["cloudy", "mostlycloudy", "partlycloudy", "cloudy"],
@@ -36,39 +37,71 @@ $(document).ready(function() {
 ////---------- BIND EVENTS
 		bindEvents: function() {
 		 $("#box1").click(function(){
-			 	App.webDetect();
-				// 	App.parseJson();
+			 App.googleLocation();
 			});
+
+			$("#cylinder1").click(function(){
+				App.parseJson();
+			 });
+
+
+				// 	App.webDetect();
+				// 	App.parseJson();
 		},
 
 
 ////---------- THE ACTUAL DOING
 
-		webDetect: function() {
 
+		googleLocation: function(){
+
+			var API_KEY = "AIzaSyAhAF8TYcKRz0GxKFJM3zzKWtgZ7PvWy3s";
+
+			var tryAPIGeolocation = function() {
+				jQuery.post( "https://www.googleapis.com/geolocation/v1/geolocate?key="+API_KEY, function(pos) {
+					console.log('latitude', pos.location.lat);
+					console.log('longitude', pos.location.lng);
+					var latitude = pos.location.lat;
+					var longitude = pos.location.lng;
+					App.wundergroundIt(latitude, longitude);
+
+			  })
+			  .fail(function(err) {
+			    alert("API Geolocation error! \n\n"+err);
+			  });
+			};
+			tryAPIGeolocation();
+		},
+
+
+		webDetect: function() {
 			navigator.geolocation.getCurrentPosition(function(pos) {
 			console.log('latitude', pos.coords.latitude);
 			console.log('longitude', pos.coords.longitude);
 			var latitude = pos.coords.latitude;
 			var longitude = pos.coords.longitude;
-
-			$.ajax({
-			url : "http://api.wunderground.com/api/341eced3dbad667b/geolookup/conditions/q/" + latitude + "," + longitude +".json",
-			dataType : "jsonp",
-			success : function(parsed_json) {
-							var location = parsed_json.location.city;
-							var icon = parsed_json.current_observation.icon;
-							console.log("icon is" + icon);
-							App.showWhat(location, icon);
-					}
 			});
 
-	});
+			App.wundergroundIt(latitude, longitude);
 
+},
+
+		wundergroundIt: function(latitude, longitude){
+				$.ajax({
+				url : "http://api.wunderground.com/api/341eced3dbad667b/geolookup/conditions/q/" + latitude + "," + longitude +".json",
+				dataType : "jsonp",
+				success : function(parsed_json) {
+								var location = parsed_json.location.city;
+								var icon = parsed_json.current_observation.icon;
+								console.log("icon is" + icon);
+								App.showWhat(location, icon);
+						}
+				});
 },
 
 		parseJson: function(){
 			$.ajax({
+				// need to get this out of cedar rapids!!
   		url : "http://api.wunderground.com/api/341eced3dbad667b/geolookup/conditions/q/IA/Cedar_Rapids.json",
 		  dataType : "jsonp",
 			success : function(parsed_json) {
