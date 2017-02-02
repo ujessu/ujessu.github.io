112758a640eaa1e468fecc1e0540855761a3a0dc
Jessica Yu <jessica.yu@gmail.com>
2017-02-01 21:25:53 -0500

    updating with color sky and new weather API

--- Aframe1/js/app.js
+++ Aframe1/js/app.js
@@ -2,16 +2,18 @@ $(document).ready(function() {
 // var aframeparticle = require('aframe-particle-system-component');
 ////---------- CONSTANTS
 	var CONSTANTS = {
-		BASE_URL: "http://api.wunderground.com/api/341eced3dbad667b/geolookup/conditions/q/IA/",
+		API_KEY: "709b6234721f968e57dd7cc4e61a1469",
+		BASE_URL: "http://api.openweathermap.org/data/2.5/weather",
 		GOOGLE_URL:"https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyAhAF8TYcKRz0GxKFJM3zzKWtgZ7PvWy3s",
 
 
-//arrays of weather icons displayed on wunderground and what i will display. structured so i can move them around, make more display options later
-		showCloud: ["cloudy", "mostlycloudy", "partlycloudy", "cloudy"],
-		showGray: ["fog", "hazy", "unknown"], //fix unknown later
-		showRain: ["chancerain", "chancesleet", "chancetstorms", "sleet", "rain", "tstorms"],
-		showSnow: ["chanceflurries", "chancesnow", "flurries", "snow"],
-		showSun: ["clear", "mostlysunny", "partlysunny", "sunny"],
+
+//arrays of weather icons displayed on OPENWEATHERMAP and what i will display. structured so i can move them around, make more display options later
+		showCloud: ["02d", "02n", "03d", "03n", "04d", "04n"],
+		showGray: ["50d", "50n"],
+		showRain: ["09d", "09n", "10d", "10n", "11d", "11n"],
+		showSnow: ["13d", "13n"],
+		showSun: ["01d", "01n"],
 
 		skyIs: ""
 	};
@@ -29,7 +31,6 @@ $(document).ready(function() {
 ////---------- APP
 	var App = {
 		init: function() {
-
 			App.bindEvents();
 		},
 
@@ -40,8 +41,9 @@ $(document).ready(function() {
 			 App.googleLocation();
 			});
 
+//sydney http://openweathermap.org/help/city_list.txt
 			$("#cylinder1").click(function(){
-				App.parseJson();
+				App.openWeathermap(-35.283459, 149.128067);
 			 });
 
 
@@ -52,19 +54,17 @@ $(document).ready(function() {
 
 ////---------- THE ACTUAL DOING
 
+//find out where user is based on browser
 
 		googleLocation: function(){
-
 			var API_KEY = "AIzaSyAhAF8TYcKRz0GxKFJM3zzKWtgZ7PvWy3s";
-
 			var tryAPIGeolocation = function() {
 				jQuery.post( "https://www.googleapis.com/geolocation/v1/geolocate?key="+API_KEY, function(pos) {
 					console.log('latitude', pos.location.lat);
 					console.log('longitude', pos.location.lng);
 					var latitude = pos.location.lat;
 					var longitude = pos.location.lng;
-					App.wundergroundIt(latitude, longitude);
-
+					App.openWeathermap(latitude, longitude);
 			  })
 			  .fail(function(err) {
 			    alert("API Geolocation error! \n\n"+err);
@@ -73,67 +73,101 @@ $(document).ready(function() {
 			tryAPIGeolocation();
 		},
 
+// //this is the old web detect function. doesn't work b/c of some sort of deprecation
+// 		webDetect: function() {
+// 			navigator.geolocation.getCurrentPosition(function(pos) {
+// 			console.log('latitude', pos.coords.latitude);
+// 			console.log('longitude', pos.coords.longitude);
+// 			var latitude = pos.coords.latitude;
+// 			var longitude = pos.coords.longitude;
+// 			});
+//
+// 			App.wundergroundIt(latitude, longitude);
+//
+// },
+//
 
-		webDetect: function() {
-			navigator.geolocation.getCurrentPosition(function(pos) {
-			console.log('latitude', pos.coords.latitude);
-			console.log('longitude', pos.coords.longitude);
-			var latitude = pos.coords.latitude;
-			var longitude = pos.coords.longitude;
+		openWeathermap: function(latitude, longitude){
+			var baseURL =
+					"http://api.openweathermap.org/data/2.5/weather";
+			var API_KEY = "709b6234721f968e57dd7cc4e61a1469";
+			var request = $.ajax(baseURL, {
+					data: {
+							lat: latitude,
+							lon: longitude,
+							appid: API_KEY
+					},
+					dataType: "json"
 			});
 
-			App.wundergroundIt(latitude, longitude);
-
-},
-
-		wundergroundIt: function(latitude, longitude){
-				$.ajax({
-				url : "http://api.wunderground.com/api/341eced3dbad667b/geolookup/conditions/q/" + latitude + "," + longitude +".json",
-				dataType : "jsonp",
-				success : function(parsed_json) {
-								var location = parsed_json.location.city;
-								var icon = parsed_json.current_observation.icon;
-								console.log("icon is" + icon);
-								App.showWhat(location, icon);
-						}
+			request.done(function(response) {
+					var location = response.name;
+					var icon = response.weather[0].icon;
+					App.showWhat(location, icon);
 				});
-},
 
-		parseJson: function(){
-			$.ajax({
-				// need to get this out of cedar rapids!!
-  		url : "http://api.wunderground.com/api/341eced3dbad667b/geolookup/conditions/q/IA/Cedar_Rapids.json",
-		  dataType : "jsonp",
-			success : function(parsed_json) {
-						  var location = parsed_json.location.city;
-						  // var temp_f = parsed_json.current_observation.temp_f;
-							var icon = parsed_json.current_observation.icon;
-							console.log("icon is" + icon);
-							App.showWhat(location, icon);
-		  		}
-  		});
+
 		},
+
 
 		showWhat: function(location, icon){
 					if (CONSTANTS.showCloud.indexOf(icon) >= 0) {
-						skyIs = "#showCloud";
+						App.makeCloud(location, icon);
 					} else if (CONSTANTS.showGray.indexOf(icon) >= 0) {
-						skyIs = "#showCloud";
+						App.makeGray(location, icon);
 					} else if (CONSTANTS.showRain.indexOf(icon) >= 0) {
-						skyIs = "#showCloud";
+						App.makeRain(location, icon);
 					} else if (CONSTANTS.showSnow.indexOf(icon) >= 0) {
-						skyIs = "#showDark";
+						App.makeSnow(location, icon);
 					} else if (CONSTANTS.showSun.indexOf(icon) >= 0) {
-						skyIs = "#showSun";
+						App.makeSun(location, icon);
 					} else {
 					alert("I dunno. You should call a friend in " + location + " to find out!");
 					}
 
-					$('#sky').attr('src',skyIs);
-					console.log("Current weather in " + location + " is " + icon + " so I am setting the sky to " + skyIs);
+		},
+
+		//
+		// <!-- <a-entity position="0 2.25 -15" particle-system="preset: snow"></a-entity> -->
+		// 			<!-- <a-entity position="0 2.25 -15" particle-system="preset: rain"></a-entity> -->
+		// <!-- <a-entity position="0 2.25 -15" particle-system="color: #EF0000,#44CC00"></a-entity> -->
+
+		makeCloud: function(location, icon){
+				$('#sky').attr('color','#d1e0e0');
+				$('#precip').attr('particle-system','preset: rain');
+				console.log("Current weather in " + location + " is " + icon + " so I need to load some clouds!!!");
+		},
+
+		makeGray: function(location, icon){
+				$('#sky').attr('color','#aaaeaf');
+				$('#precip').attr('particle-system','');
+				console.log("Current weather in " + location + " is " + icon + " so I am making the sky gray");
+		},
+
+		makeRain: function(location, icon){
+				$('#sky').attr('color','#487a89');
+				$('#precip').attr('position', '0 2.25 -15');
+				$('#precip').attr('particle-system','preset: rain');
+				console.log("Current weather in " + location + " is " + icon + " so I need to load rain");
+		},
+
+		makeSnow: function(location, icon){
+				$('#sky').attr('color','#d1e0e0');
+				$('#precip').attr('position', '0 2.25 -15');
+				$('#precip').attr('particle-system','preset: snow');
+				console.log("Current weather in " + location + " is " + icon + " so I need to load snow");
+		},
+
+		makeSun: function(location, icon){
+				$('#sky').attr('color','#89e3ff');
+				$('#precip').attr('particle-system','');
+				console.log("Current weather in " + location + " is " + icon + " so I am making the sky blue");
 		}
 
+
+
 };
+
 
 ///----------App init
 	App.init();
